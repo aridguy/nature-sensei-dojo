4baa537875d5d1540292eecaf8491552b37e5355
aridguy <rotimiariyo@gmail.com>
2022-10-07 19:50:43 +0100

    added some bootstrap files

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
+import "./Navbar.css"
+
 const Navbar = () => {
-    return(
+    return (
         <div>
-            <nav className='navbar'>
-            <div className='container-fluid'></div>
-                
+            <div className='container-fluid bg-primary topnav'>
+                <div className='container'>
+                    <div className='contents row'>
+                        <div className='col-md-3'>
+                            <i className='fa fa-phone text-theme-colored'></i>
+                        </div>
+                        <div className='col-md-3'></div>
+                        <div className='col-md-3'></div>
+                        <div className='col-md-3'></div>
+                    </div>
+                </div>
+            </div>
+            <nav className='navbar container-fluid'>
+                <div className='container'>
+
+                </div>
             </nav>
-        
-        
         </div>
     )
 }
