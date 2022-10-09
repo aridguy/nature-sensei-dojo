a704100e1707ac09eba692b85988c685002ea741
aridguy <rotimiariyo@gmail.com>
2022-10-09 23:52:00 +0100

    starting the project for today this is the previous stop commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,27 +9,38 @@ const Navbar = () => {
             <div className='container-fluid bg-primary topnav'>
                 <div className='container'>
                     <div className='contents row'>
-                        <div className='col-md-3'>
-                            <i className='fa fa-phone text-theme-colored'> 234 0945 575 565</i>
+                        <div className='col-md-7'>
+                            <i className='fa fa-phone text-theme-colored'> 234 0945 575 565</i>&nbsp;&nbsp;
+                            <i className='fa fa-clock-o text-theme-colored'> Mon-Fri 8:00 to 2:00</i>&nbsp;&nbsp;
+                            <i className='fa fa-envelope-o text-theme-colored'> contact@yourdomain.com</i>
                         </div>
-                        <div className='col-md-3'>
-                            <i className='fa fa-clock-o text-theme-colored'>Mon-Fri 8:00 to 2:00</i>
-                        </div>
-                        <div className='col-md-3'>
-
-                            <i className='fa fa-envelope-o text-theme-colored'>sensei@mail.com</i>
-                        </div>
-                        <div className='col-md-3'>
-                        <i className='fa fa-clock-o text-theme-colored'>Mon-Fri 8:00 to 2:00</i>
+                        <div className='col-md-5'>
+                            <i className='fa fa-facebook'></i>
+                            <i className='fa fa-google-plus'></i>
+                            <i className='fa fa-instagram'></i>
+                            <i className='fa fa-twitter'></i>
                         </div>
                     </div>
                 </div>
             </div>
-            <nav className='navbar container-fluid'>
-                <div className='container'>
 
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">WebSiteName</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
                 </div>
             </nav>
+
         </div>
     )
 }
