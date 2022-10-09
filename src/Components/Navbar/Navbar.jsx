9d2f5abab20cda7693ffdfbb2ec28763ff69e67c
aridguy <rotimiariyo@gmail.com>
2022-10-09 22:22:07 +0100

    starting the project for today this is the previous stop commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,15 +5,23 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <div>
+
             <div className='container-fluid bg-primary topnav'>
                 <div className='container'>
                     <div className='contents row'>
                         <div className='col-md-3'>
-                            <i className='fa fa-phone text-theme-colored'></i>
+                            <i className='fa fa-phone text-theme-colored'> 234 0945 575 565</i>
                         </div>
-                        <div className='col-md-3'></div>
-                        <div className='col-md-3'></div>
-                        <div className='col-md-3'></div>
+                        <div className='col-md-3'>
+                            <i className='fa fa-clock-o text-theme-colored'>Mon-Fri 8:00 to 2:00</i>
+                        </div>
+                        <div className='col-md-3'>
+
+                            <i className='fa fa-envelope-o text-theme-colored'>sensei@mail.com</i>
+                        </div>
+                        <div className='col-md-3'>
+                        <i className='fa fa-clock-o text-theme-colored'>Mon-Fri 8:00 to 2:00</i>
+                        </div>
                     </div>
                 </div>
             </div>
