aefaabfbbad9d943a6a0e135c951f0f2dc992861
aridguy <rotimiariyo@gmail.com>
2022-10-17 01:07:29 +0100

    done the navbar fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,49 +1,63 @@
 import React from 'react'
 
 import "./Navbar.css"
+import Logos from '../../Assets/Logo.png'
 
 const Navbar = () => {
-    return (
-        <div>
+  return (
+    <div>
 
-            <div className='container-fluid bg-primary topnav'>
-                <div className='container'>
-                    <div className='contents row'>
-                        <div className='col-md-7'>
-                            <i className='fa fa-phone text-theme-colored'> 234 0945 575 565</i>&nbsp;&nbsp;
-                            <i className='fa fa-clock-o text-theme-colored'> Mon-Fri 8:00 to 2:00</i>&nbsp;&nbsp;
-                            <i className='fa fa-envelope-o text-theme-colored'> contact@yourdomain.com</i>
-                        </div>
-                        <div className='col-md-5'>
-                            <i className='fa fa-facebook'></i>
-                            <i className='fa fa-google-plus'></i>
-                            <i className='fa fa-instagram'></i>
-                            <i className='fa fa-twitter'></i>
-                        </div>
-                    </div>
-                </div>
-                <p className='close'>X</p>
+      <div className='container-fluid bg-primary topnav'>
+        <div className='container'>
+          <div className='contents row'>
+            <div className='col-md-7'>
+              <i className='fa fa-phone text-theme-colored'> 234 0945 575 565</i>&nbsp;&nbsp;
+              <i className='fa fa-clock-o text-theme-colored'> Mon-Fri 8:00 to 2:00</i>&nbsp;&nbsp;
+              <i className='fa fa-envelope-o text-theme-colored'> contact@yourdomain.com</i>
             </div>
-
-            <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="#">WebSiteName</a>
-                    </div>
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Page 1</a></li>
-                        <li><a href="#">Page 2</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                    </ul>
-                </div>
-            </nav>
-
+            <div className='col-md-5'>
+              <i className='fa fa-facebook'></i>
+              <i className='fa fa-google-plus'></i>
+              <i className='fa fa-instagram'></i>
+              <i className='fa fa-twitter'></i>
+            </div>
+          </div>
         </div>
-    )
+        <p className='close'>X</p>
+      </div>
+      <header>
+      <div className="nav">
+          <input type="checkbox" id="nav-check" />
+          <div className="nav-header">
+              <div className="nav-title">
+                  <img className="brand" src={Logos} alt="Logo" />
+              </div>
+          </div>
+          <div className="nav-btn">
+              <label for="nav-check">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+              </label>
+          </div>
+          <div className="nav-links navigationbar">
+              <li >Home</li>
+              <li id="#aboutUs">About</li>
+              <li>What We Do</li>
+              <li>Vision/Mission</li>
+              <li>Contact Us</li>
+
+              <li>
+                  <button className="btn btn-warning donate">Volunteer</button>
+              </li>
+          </div>
+      </div>
+  </header>
+        
+         
+      
+    </div>
+  )
 }
 
 export default Navbar
